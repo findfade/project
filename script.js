f989479f5dbf71b2c5d7a4909f571ba269cbb336
Pavel <pasik0443@gmail.com>
2020-12-28 20:53:17 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+
+
+
+console.log("Hello");
+let $numberOne = 5;
+let _numderTwo = 2;
+let result = $numberOne + _numderTwo;
+alert(result);
+const numberTree = 10;
+console.log(numberTree);
