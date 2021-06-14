3cd1804ceb3079052c9bd7d88f03a70ef614fdcd
Pavel <pasik0443@gmail.com>
2021-06-14 14:02:23 +0400

    циклы и условия (тренировка)

---
+++ study.js
@@ -0,0 +1,38 @@
+"use strict";
+
+let num = 50;
+//(num === 50) ? console.log("good") : console.log("not");
+
+//switch (num) {
+//    case "49": 
+//        console.log("49");
+//        break;
+//    case "100":
+//       console.log("100");
+//        break;   
+//    case "460":
+//        console.log("460");
+//        break;
+//    default:
+//       console.log("noo");
+//        break;
+//}
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do { 
+//     console.log(num);
+//     num++;
+// }
+// while (num <55);
+
+// for (let i = 1; i < 15; i++) {
+//     if (i=== 6){
+//         //break;
+//         continue;
+//     }
+//     console.log(i);
+// }
